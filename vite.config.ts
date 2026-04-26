5dcfe20fb30528f834cc51555535a6f4c9143b76
sazzycooltehc <87143944+sazzycooltehc@users.noreply.github.com>
2026-04-26 21:28:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  base: '/Project-akhand/',
   return {
     plugins: [react(), tailwindcss()],
     define: {
